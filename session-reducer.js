6ba3562cc5e1dce4eda08923c2e06b147dbe1e1e
Edward Seufert <edwardseufert@Edwards-MBP.home>
2019-08-03 21:30:48 -0400

    update security

--- session-reducer.js
+++ session-reducer.js
@@ -1,39 +1,29 @@
 export default function sessionReducer(state = {}, action) {
-  let myState = {};
-  switch(action.type) {
-    case 'LOAD_SESSION_CHECK': {
-      if (action.responseJson != null) {
-        if (action.responseJson.params != null) {
-          if (action.responseJson.params.status != null) {
-            let sessionInfos = action.responseJson.params.status.info;
-            for (let j = 0; j < sessionInfos.length; j++) {
-              if (sessionInfos[j].code === "success" && sessionInfos[j].message === "Alive") {
-                myState.sessionActive = true;
-              }
-            }
-          }
-        } else if (action.responseJson.status != null && action.responseJson.status == 401) {
-          myState.sessionActive = false;
-        }
-      }
-      return Object.assign({}, state, myState);
-    }
-    case 'SAVE_AUTHENTICATION': {
-      if (action.responseJson != null && action.responseJson.params != null
-        && action.responseJson.params.status != null && action.responseJson.params.status.info != null) {
-        let infos = action.responseJson.params.status.info;
-        for (var i = 0; i < infos.length; i++) {
-          if (infos[i].code === "success" && infos[i].message === "Authenticated") {
-            myState.sessionActive = true;
-          }
-        }
-      }
-      return Object.assign({}, state, myState);
-    }
-    case 'PROCESS_LOGOUT': {
-      return Object.assign({}, state, {sessionActive:false});
-    }
-    default:
-      return state;
-    }
-  }
+	let myState = {};
+	switch(action.type) {
+	case 'LOAD_SESSION_CHECK': {
+		if (action.responseJson != null && action.responseJson.status != null && action.responseJson.status === "SUCCESS") {
+			myState.sessionActive = true;
+		} else {
+			myState.sessionActive = false;
+			myState.user = null;
+		}
+		return Object.assign({}, state, myState);
+	}
+	case 'SAVE_SESSION': {
+		if (action.responseJson != null && action.responseJson.params != null && action.responseJson.params.USER != null) {
+			return Object.assign({}, state, {sessionActive:true, status:'JUST_LOGGEDIN', user:action.responseJson.params.USER});
+		} else {
+			return state;
+		}
+	}
+	case 'CLEAR_SESSION_LOGIN': {
+		return Object.assign({}, state, {status:''});
+	}
+	case 'PROCESS_LOGOUT': {
+		return Object.assign({}, state, {sessionActive:false, user:null});
+	}
+	default:
+		return state;
+	}
+}
